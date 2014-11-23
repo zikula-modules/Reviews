522f2b1a8fca1afc847a21f7e4549c07553f19b7
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2014-11-23 22:57:42 +0100

    fixed #24

--- lib/Reviews/Api/User.php
+++ lib/Reviews/Api/User.php
@@ -34,6 +34,13 @@ class Reviews_Api_User extends Zikula_AbstractApi
 
         if (in_array('review', $allowedObjectTypes)
                 && SecurityUtil::checkPermission($this->name . ':Review:', '::', ACCESS_READ)) {
+            $links[] = array('url' => ModUtil::url($this->name, 'user', 'main'),
+                    'text' => $this->__('Reviews index'),
+                    'title' => $this->__('Index'));
+        }
+
+        if (in_array('review', $allowedObjectTypes)
+                && SecurityUtil::checkPermission($this->name . ':Review:', '::', ACCESS_READ)) {
             $links[] = array('url' => ModUtil::url($this->name, 'user', 'view', array('ot' => 'review')),
                     'text' => $this->__('Reviews'),
                     'title' => $this->__('Review list'));
@@ -111,17 +118,17 @@ class Reviews_Api_User extends Zikula_AbstractApi
         if (!isset($args['args'][$args['func'] . 'ending'])) {
             if ($args['func'] == 'view') {
                 // category list
-               /* if (isset($args['args']['prop'])) {
-                    $vars = $args['args']['prop'];
-                    if (isset($args['args']['cat'])) {
-                        $vars .= '/'.$args['args']['cat'];
-                    }
-                    // letter list
+                /* if (isset($args['args']['prop'])) {
+                 $vars = $args['args']['prop'];
+                if (isset($args['args']['cat'])) {
+                $vars .= '/'.$args['args']['cat'];
+                }
+                // letter list
                 } elseif (isset($args['args']['letter'])) {
-                    $vars = 'letter/'.$args['args']['letter'];
+                $vars = 'letter/'.$args['args']['letter'];
                 }
                 if (isset($args['args']['page']) && $args['args']['page'] != 1) {
-                    $vars .= (empty($vars) ? '' : '/').'page/'.$args['args']['page'];
+                $vars .= (empty($vars) ? '' : '/').'page/'.$args['args']['page'];
                 }*/
                 $args['args'][$args['func'] . 'ending'] = '';//'/';
             } else if ($args['func'] == 'display') {
@@ -140,7 +147,7 @@ class Reviews_Api_User extends Zikula_AbstractApi
              }
              $filterId = $args['args'][$filterField];
              unset($args['args'][$filterField]);
-              
+
              $filterGroupFolder = $routerFacade->getGroupingFolderFromObjectType($filterEntity, 'display', $args['args']);
              $filterSlug = $routerFacade->getFormattedSlug($filterEntity, 'display', $args['args'], $filterId);
              $result .= $filterGroupFolder . '/' . $filterSlug .'/';
@@ -148,6 +155,7 @@ class Reviews_Api_User extends Zikula_AbstractApi
              }
              */
         } elseif ($args['func'] == 'display') {
+
             // determine given id
             $id = 0;
             foreach (array('id', strtolower($objectType) . 'id', 'objectid') as $idFieldName) {
@@ -158,10 +166,26 @@ class Reviews_Api_User extends Zikula_AbstractApi
             }
             
             if (ModUtil::getVar('Reviews', 'addcategorytitletopermalink') == 1 && ModUtil::getVar('Reviews', 'enablecategorization') == 1) {
-                $prop = $this->request->query->filter('prop', '');
-                if ($prop != '') {
-                $args['args']['cat'] = $prop;
+                if ($id > 0) {
+                    $modelHelper = new Reviews_Util_Model($this->serviceManager);
+                    $repository = $modelHelper->getReviewRepository();
+                    $thisreview = $repository->selectById($id);
+                    $categories = $thisreview->getCategories();
+                    $name = $categories[0]->getCategory()->getName();
+                    $displayname = $categories[0]->getCategory()->getDisplayName();
                 }
+            
+                $lang = ZLanguage::getLanguageCode();
+            
+                $cat = '';
+                if ($name != '') {
+                    $cat = $name;
+                }
+                if ($displayname != '') {
+                    $cat = $displayname[$lang];
+                }
+            
+                $args['args']['cat'] = $cat;
             }
              
             // check if we have a valid slug given
@@ -206,79 +230,13 @@ class Reviews_Api_User extends Zikula_AbstractApi
         // enforce url name of the module, but do only 1 replacement to avoid changing other params
         $modInfo = ModUtil::getInfoFromName('Reviews');
         $result = preg_replace('/' . $modInfo['name'] . '/', $modInfo['url'], $result, 1);
+
+        $result = preg_replace('#'. 'review/' . '#', '', $result, 1);
+        
+        $result = preg_replace('=' . '\+' . '=', ' ' , $result);
          
         return $result;
-         
-        /////////////Old code
-         
-        // check we have the required input
-        /*  if (!isset($args['modname']) || !isset($args['func']) || !isset($args['args'])) {
-         return LogUtil::registerArgsError();
-        }
 
-        if (!isset($args['type'])) {
-        $args['type'] = 'user';
-        }
-
-        // create an empty string ready for population
-        $vars = '';
-
-        // view function
-        if ($args['func'] == 'view') {
-        // category list
-        if (isset($args['args']['prop'])) {
-        $vars = $args['args']['prop'];
-        if (isset($args['args']['cat'])) {
-        $vars .= '/'.$args['args']['cat'];
-        }
-        // letter list
-        } elseif (isset($args['args']['letter'])) {
-        $vars = 'letter/'.$args['args']['letter'];
-        }
-        if (isset($args['args']['page']) && $args['args']['page'] != 1) {
-        $vars .= (empty($vars) ? '' : '/').'page/'.$args['args']['page'];
-        }
-        }*/
-
-        // for the display function use either the title (if present) or the page id
-        if ($args['func'] == 'display') {
-            // check for the generic object id parameter
-            /* if (isset($args['args']['objectid'])) {
-             $args['args']['id'] = $args['args']['objectid'];
-            }
-            // get the item (will be cached by DBUtil)
-            if (isset($args['args']['id'])) {
-            $item = ModUtil::apiFunc('Reviews', 'selection', 'getEntity', array('id' => $args['args']['id']));
-            } else {
-            $item = ModUtil::apiFunc('Reviews', 'selection', 'getEntity', array('title' => $args['args']['title']));
-            }*/
-            /*if (ModUtil::getVar('Reviews', 'addcategorytitletopermalink') && isset($args['args']['cat'])) {
-                $vars = $args['args']['cat'].'/'.$item['slug'];
-            } else {
-            $vars = $item['slug'];
-            }
-            if (isset($args['args']['page']) && $args['args']['page'] != 1) {
-            $vars .= '/page/'.$args['args']['page'];
-            }*/
-        }
-
-        // don't display the function name if either displaying an page or the normal overview
-        if ($args['func'] == 'main' || $args['func'] == 'display') {
-            $args['func'] = '';
-        }
-
-        // construct the custom url part
-        if (empty($args['func']) && empty($vars)) {
-         return $args['modname'] . '/';
-        } elseif (empty($args['func'])) {
-        return $args['modname'] . '/' . $vars . '/';
-        } elseif (empty($vars)) {
-        return $args['modname'] . '/' . $args['func'] . '/';
-        } else {
-        return $args['modname'] . '/' . $args['func'] . '/' . $vars . '/';
-        }
-
-        return $result;
     }
 
     /**
@@ -318,7 +276,7 @@ class Reviews_Api_User extends Zikula_AbstractApi
          
         // remove some unrequired parameters
         foreach ($_GET as $k => $v) {
-            if (in_array($k, array('module', 'type', 'func', 'lang', 'ot')) === false) {
+            if (in_array($k, array('module', 'type', 'func', 'lang', 'ot', 'prop', 'cat')) === false) {
                 unset($_GET[$k]);
             }
         }
@@ -386,99 +344,5 @@ class Reviews_Api_User extends Zikula_AbstractApi
         }
          
         return true;
-
-        ///////////////// old code
-         
-        // check we actually have some vars to work with...
-        /*if (!isset($args['vars'])) {
-         return LogUtil::registerArgsError();
-        }*/
-
-        // define the available user functions
-        //$funcs = array('main', 'view', 'display', 'edit');
-        // set the correct function name based on our input
-        if (empty($args['vars'][2])) {
-            System::queryStringSetVar('func', 'main');
-        } elseif (!in_array($args['vars'][2], $funcs)) {
-            System::queryStringSetVar('func', 'display');
-            $nextvar = 2;
-        } else {
-            System::queryStringSetVar('func', $args['vars'][2]);
-            $nextvar = 3;
-        }
-
-        // check the list function
-        if (FormUtil::getPassedValue('func') == 'view' && isset($args['vars'][$nextvar])) {
-            // get rid of unused vars
-            $args['vars'] = array_slice($args['vars'], $nextvar);
-
-            // check if the letter parameter is present
-            if ($args['vars'][0] == 'letter') {
-                System::queryStringSetVar('letter', (string)$args['vars'][1]);
-                $nextvar = 2;
-            } elseif ($args['vars'][0] == 'page') {
-                System::queryStringSetVar('page', (int)$args['vars'][1]);
-                $nextvar = 0;
-            } else {
-                // add the category info
-                System::queryStringSetVar('prop', (string)$args['vars'][0]);
-                $nextvar = 1;
-
-                if (isset ($args['vars'][1])) {
-                    // check if there's a page arg
-                    $varscount = count($args['vars']);
-                    ($args['vars'][$varscount-2] == 'page') ? $pagersize = 2 : $pagersize = 0;
-                    // extract the category path
-                    $cat = implode('/', array_slice($args['vars'], 1, $varscount - $pagersize - 1));
-                    System::queryStringSetVar('cat', $cat);
-                    $nextvar = 2;
-                }
-            }
-            if (isset($args['vars'][$nextvar]) && $nextvar != 0 && $args['vars'][$nextvar] == 'page') {
-                System::queryStringSetVar('page', (int)$args['vars'][$nextvar+1]);
-            }
-        }
-
-        // identify the correct parameter to identify the page
-        if (FormUtil::getPassedValue('func') == 'display') {
-            // get rid of unused vars
-            $args['vars'] = array_slice($args['vars'], $nextvar);
-            $nextvar = 0;
-            // remove any category path down to the leaf category
-            $varscount = count($args['vars']);
-            if (ModUtil::getVar('Reviews', 'addcategorytitletopermalink') && !empty($args['vars'][$nextvar+1])) {
-                ($args['vars'][$varscount-2] == 'page') ? $pagersize = 2 : $pagersize = 0;
-                $category = array_slice($args['vars'], 0, $varscount - 1 - $pagersize);
-                System::queryStringSetVar('cat', implode('/',$category));
-                array_splice($args['vars'], 0,  $varscount - 1 - $pagersize);
-            }
-            if (is_numeric($args['vars'][$nextvar])) {
-                System::queryStringSetVar('id', $args['vars'][$nextvar]);
-            } else {
-                System::queryStringSetVar('title', $args['vars'][$nextvar]);
-            }
-            $nextvar++;
-            if (isset($args['vars'][$nextvar]) && $args['vars'][$nextvar] == 'page') {
-                System::queryStringSetVar('page', (int)$args['vars'][$nextvar+1]);
-            }
-        }
-
-        return true;
     }
-
-    /**
-     * get meta data for the module
-     */
-    /* public function getmodulemeta()
-     {
-    return array('viewfunc'    => 'view',
-            'displayfunc' => 'display',
-            'newfunc'     => 'new',
-            'createfunc'  => 'create',
-            'modifyfunc'  => 'modify',
-            'updatefunc'  => 'update',
-            'deletefunc'  => 'delete',
-            'titlefield'  => 'title',
-            'itemid'      => 'id');
-    }*/
 }
--- lib/Reviews/RouterFacade.php
+++ lib/Reviews/RouterFacade.php
@@ -22,7 +22,7 @@ class Reviews_RouterFacade extends Reviews_Base_RouterFacade
     function __construct()
     {
         $displayDefaultEnding = System::getVar('shorturlsext', '');
-    
+
         $this->requirements = array(
                 'func'          => '\w+',
                 'ot'            => '\w+',
@@ -31,14 +31,50 @@ class Reviews_RouterFacade extends Reviews_Base_RouterFacade
                 'viewending'    => '(?:\.csv|\.rss|\.atom|\.xml|\.pdf|\.json|\.kml)?',
                 'id'            => '\d+'
         );
-    
+
         // initialise and reference router instance
         $this->router = new Zikula_Routing_UrlRouter();
-    
+
         // add generic routes
         return $this->initUrlRoutes();
     }
-        
+    
+    /**
+     * Initialise the url routes for this application.
+     *
+     * @return Zikula_Routing_UrlRouterUrlRouter The router instance treating all initialised routes
+     */
+    protected function initUrlRoutes()
+    {
+        $fieldRequirements = $this->requirements;
+        $isDefaultModule = (System::getVar('shorturlsdefaultmodule', '') == 'Reviews');
+    
+        $defaults = array();
+        $modulePrefix = '';
+        if (!$isDefaultModule) {
+            $defaults['module'] = 'Reviews';
+            $modulePrefix = ':module/';
+        }
+    
+        $defaults['func'] = 'view';
+        $viewFolder = 'view';
+        // normal views (e.g. orders/ or customers.xml)
+        $this->router->set('va', new Zikula_Routing_UrlRoute($modulePrefix . $viewFolder . '/:ot:viewending', $defaults, $fieldRequirements));
+    
+        // TODO filter views (e.g. /orders/customer/mr-smith.csv)
+        // $this->initRouteForEachSlugType('vn', $modulePrefix . $viewFolder . '/:ot/:filterot/', ':viewending', $defaults, $fieldRequirements);
+    
+        $defaults['func'] = 'display';
+        // normal display pages including the group folder corresponding to the object type
+        $this->initRouteForEachSlugType('dn', $modulePrefix . ':ot/', ':displayending', $defaults, $fieldRequirements);
+    
+        // additional rules for the leading object type (where ot is omitted)
+        $defaults['ot'] = 'review';
+        $this->initRouteForEachSlugType('dl', $modulePrefix . '', ':displayending', $defaults, $fieldRequirements);
+    
+        return $this->router;
+    }
+
     /**
      * Helper function to route permalinks for different slug types.
      *
@@ -51,10 +87,48 @@ class Reviews_RouterFacade extends Reviews_Base_RouterFacade
     protected function initRouteForEachSlugType($prefix, $patternStart, $patternEnd, $defaults, $fieldRequirements)
     {
         // entities with unique slug (slug only)
-        $this->router->set($prefix . 'a', new Zikula_Routing_UrlRoute($patternStart . ':slug' . $patternEnd,        $defaults, $fieldRequirements));
+        $categoryenabled = ModUtil::getVar('Reviews', 'enablecategorization');
+        $cattopermalink = ModUtil::getVar('Reviews', 'addcategorytitletopermalink');
+        if ($categoryenabled == 1 && $cattopermalink == 1) {
+            $this->router->set($prefix . 'a', new Zikula_Routing_UrlRoute($patternStart . ':cat' . '/' . ':slug' . $patternEnd,        $defaults, $fieldRequirements));
+        } else {
+            $this->router->set($prefix . 'b', new Zikula_Routing_UrlRoute($patternStart . ':slug' . $patternEnd,        $defaults, $fieldRequirements));
+        }
         // entities with non-unique slug (slug and id)
-        $this->router->set($prefix . 'b', new Zikula_Routing_UrlRoute($patternStart . ':slug.:id.' . $patternEnd,    $defaults, $fieldRequirements));
+        $this->router->set($prefix . 'c', new Zikula_Routing_UrlRoute($patternStart . ':slug.:id.' . $patternEnd,    $defaults, $fieldRequirements));
         // entities without slug (id)
-        $this->router->set($prefix . 'c', new Zikula_Routing_UrlRoute($patternStart . 'id.:id.' . $patternEnd,        $defaults, $fieldRequirements));
+        $this->router->set($prefix . 'd', new Zikula_Routing_UrlRoute($patternStart . 'id.:id.' . $patternEnd,        $defaults, $fieldRequirements));
+    }
+
+    /**
+     * Get name of grouping folder for given object type and function.
+     *
+     * @param string $objectType Name of treated entity type.
+     * @param string $func       Name of function.
+     *
+     * @return string Name of the group folder
+     */
+    public function getGroupingFolderFromObjectType($objectType, $func)
+    {
+        // object type will be used as a fallback
+        $groupFolder = $objectType;
+
+        if ($func == 'view') {
+            switch ($objectType) {
+                case 'review':
+                    $groupFolder = 'reviews';
+                    break;
+                default: return '';
+            }
+        } else if ($func == 'display') {
+            switch ($objectType) {
+                case 'review':
+                    $groupFolder = 'review';
+                    break;
+                default: return '';
+            }
+        }
+
+        return $groupFolder;
     }
 }
